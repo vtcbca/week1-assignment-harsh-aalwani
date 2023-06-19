f33da64eaa54bb36ab4420ee4e48d65ffeeb2a4f
harsh-aalwani <122863660+harsh-aalwani@users.noreply.github.com>
2023-06-19 10:53:35 +0530

    Delete solution1.py

--- Week1/solution1.py
+++
@@ -1,6 +0,0 @@
-#write python script to enter any number and check whether its prime or not
-n=int(input('Enter a number:'))
-if(n/2==0 or n/3==0 or n/3==0 or n/5==0 or n/7==0 or n/13==0):
-    print(n,' is prime number.')
-else:
-    print(n,' is not prime number.')
